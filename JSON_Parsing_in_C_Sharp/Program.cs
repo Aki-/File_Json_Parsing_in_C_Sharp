b84137a65da875883c9716ab06857f28d4560deb
Aki- <akshaydalvi24@gmail.com>
2019-08-01 18:18:18 +0530

    Add project files.

---
+++ JSON_Parsing_in_C_Sharp/Program.cs
@@ -0,0 +1,59 @@
+﻿using Newtonsoft.Json;
+using Newtonsoft.Json.Linq;
+using System;
+using System.IO;
+
+namespace JSON_Parsing_in_C_Sharp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //Load and Read json file using StreamReader
+
+
+            String path = "./InputDoc/Expense.json";
+
+            using (StreamReader sr = File.OpenText(path))
+            {
+
+                var json = sr.ReadToEnd();
+
+                JArray obj = JsonConvert.DeserializeObject<JArray>(json);
+                foreach (var result in obj)
+                {
+
+                    //first loop
+                    string WHO = (string)result["WHO"];
+                    Console.WriteLine("\n \n *** User:- " + WHO + " ");
+
+
+                    //second loop
+                    foreach (JObject user in result["WEEK"])
+                    {
+                        string NUMBER = (string)user["NUMBER"];
+                        Console.WriteLine("\n Week number:- " + NUMBER + " ");
+
+                        //third loop
+                        foreach (JObject expns in user["EXPENSE"])
+                        {
+                            string ExpFor = (string)expns["WHAT"];
+                            string ExpAmt = (string)expns["AMOUNT"];
+                            Console.WriteLine("\t Expensed for:- " + ExpFor + ", Amount spent: " + ExpAmt + " ");
+
+                        }
+
+
+                    }
+
+
+
+
+                }
+
+
+                Console.Read();
+            }
+        }
+    }
+}
---
+++ JSON_Parsing_in_C_Sharp/RootObject.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace JSON_Parsing_in_C_Sharp
+{
+    class RootObject
+    {
+        public class Rootobject
+        {
+            public Class1[] Property1 { get; set; }
+        }
+
+        public class Class1
+        {
+            public string WHO { get; set; }
+            public WEEK[] WEEK { get; set; }
+        }
+
+        public class WEEK
+        {
+            public int NUMBER { get; set; }
+            public EXPENSE[] EXPENSE { get; set; }
+        }
+
+        public class EXPENSE
+        {
+            public string WHAT { get; set; }
+            public float AMOUNT { get; set; }
+        }
+    }
+}
